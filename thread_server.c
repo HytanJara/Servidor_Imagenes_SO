7674f93e8e02cd7a2d211c92913c1dba320abc98
JhonnASC <jhonnsolis@gmail.com>
2025-04-13 23:52:16 -0600

    [FIX] removing not necessary includes

--- thread_server.c
+++ thread_server.c
@@ -7,17 +7,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include "thread_server.h"
 #include "log_utils.h" // Para logging
-#include <unistd.h> // para chdir()
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 
 #define PORT 8080
 #define BUFFER_SIZE 4096
